e1f285805cfdce1cc92f6a58108acdfe5ca6b138
Jenil Shah <159883258+Jenil6743@users.noreply.github.com>
2026-04-15 11:15:30 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sorted].append(s)
+        return list(res.values())
---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left , right = 0 , len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+            
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[1] <= nums[mid]:
+                if target > nums[mid] or target < nums[1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1 
+
+            else:
+                if target < nums[mid] or target > nums[1]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+
+
+        
